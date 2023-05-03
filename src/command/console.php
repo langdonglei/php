6a4dad27bd2d39f2d72e591dc01751e150eab878
langdonglei <langdonglei@icloud.com>
2023-05-03 23:45:34 +0800

    v

--- src/command/console.php
+++ src/command/console.php
@@ -1,17 +1,15 @@
+#!/usr/bin/env php
 <?php
 
 use langdonglei\util\command\fastadmin\addon\Install;
 use langdonglei\util\command\fastadmin\addon\Zip;
 use Symfony\Component\Console\Application;
 
-
 $root = dirname(__DIR__);
-
 if (!is_file($root . '/vendor/autoload.php')) {
     $root = dirname(__DIR__, 2);
 }
-var_dump($root);exit();
-include __DIR__ . '/../../vendor/autoload.php';
+require_once $root . '/vendor/autoload.php';
 
 $application = new Application();
 $application->add(new Install());
