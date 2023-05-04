32de3918316222522298ec702708239407d22aaa
langdonglei <langdonglei@icloud.com>
2023-05-04 13:38:10 +0800

    v

--- src/command/fastadmin/addon/Zip.php
+++ src/command/fastadmin/addon/Zip.php
@@ -7,17 +7,21 @@ use langdonglei\util\library\File;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use think\console\input\Argument;
 
 class Zip extends Command
 {
     protected function configure()
     {
-        $this->setName('fastadmin:addon:zip');
+        $this
+            ->addArgument('target', Argument::REQUIRED, 'the target you will zip')
+            ->setName('fastadmin:addon:zip');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        File::unzip(ROOT_PATH . '/abc.zip');
+        $target = $input->getArgument('target');
+        File::unzip($target);
 //        File::zip(ROOT_PATH . '/abc');
         return 0;
     }
