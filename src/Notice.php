70b4b24b98da84c232109a0a562e36200d42ba1e
langdonglei <langdonglei@icloud.com>
2023-05-19 21:15:59 +0800

    notice

---
+++ src/Notice.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace langdonglei\util;
+use GuzzleHttp\Client;
+
+class Notice
+{
+    public static function WechatMediaPlatform($openid)
+    {
+        $app_id     = '';
+        $app_secret = '';
+        $str        = file_get_contents("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$app_id&secret=$app_secret");
+        $arr        = json_decode($str, true);
+        $res        = (new Client())->post("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=$arr[access_token]", [
+            'body' => [
+                'touser'      => $openid,
+                'template_id' => '',
+                'url'         => '',
+                'data'        => '',
+            ]
+        ])->getBody()->getContents();
+        return json_decode($res, true);
+    }
+}
