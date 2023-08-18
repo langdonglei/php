6ac5839c90aa6db785ac0154da4f3bf2fa123326
langdonglei <30425179+langdonglei@users.noreply.github.com>
2023-08-18 19:41:40 +0800

    v

--- src/Office.php
+++ src/Office.php
@@ -2,51 +2,27 @@
 
 namespace langdonglei;
 
+use Exception;
+use PHPExcel;
+use PHPExcel_IOFactory;
 use PhpOffice\PhpSpreadsheet\Reader\Csv;
 use PhpOffice\PhpSpreadsheet\Reader\Xls;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+use Throwable;
 
 class Office
 {
-    public function import()
+    /**
+     * @throws Throwable
+     */
+    public function excel_import($target, $fields, $model = null)
     {
-        $file = $this->request->request('file');
-        if (!$file) {
-            $this->error(__('Parameter %s can not be empty', 'file'));
-        }
-        $path = ROOT_PATH . 'public' . $file;
-        if (!is_file($path)) {
-            $this->error(__('No results were found'));
-        }
-        $excel = $this->reader($path)->load($path);
-        if (!$excel) {
-            $this->error('文件加载失败');
-        }
-        $field = ['sn', 'no', 'name', 'qc', 'cdz', 'zzzj', 'gjd', 'cs', 'zzclip', 'tbq', 'zzbkz'];
-        $data  = [];
-        $sheet = $excel->getSheet(0);
-        $row   = $sheet->getHighestRow();
-        $col   = count($field);
-        for ($r = 2; $r <= $row; $r++) {
-            $item = [];
-            for ($c = 1; $c <= $col; $c++) {
-                $item[] = $sheet->getCellByColumnAndRow($c, $r)->getValue() ?? '';
-            }
-            $data[] = array_combine($field, $item);
-        }
-        $this->model->execute('truncate vv_employee');
-        $this->model->saveAll($data);
-        $this->success('导入数据' . count($data) . '条');
-    }
-
-    private function reader($path)
-    {
-        $ext = pathinfo($path, PATHINFO_EXTENSION);
+        $ext = pathinfo($target, PATHINFO_EXTENSION);
         if (!in_array($ext, ['csv', 'xls', 'xlsx'])) {
             throw new Exception('未知的数据格式');
         }
         if ($ext === 'csv') {
-            $file = fopen($path, 'r');
+            $file = fopen($target, 'r');
             $path = tempnam(sys_get_temp_dir(), 'import_csv');
             $fp   = fopen($path, "w");
             $n    = 0;
@@ -70,17 +46,71 @@ class Office
         } else {
             $reader = new Xlsx();
         }
-        return $reader;
+        $data = [];
+        try {
+            $sheet = $reader->load($target)->getSheet(0);
+            $row   = $sheet->getHighestRow();
+            $col   = count($fields);
+            for ($r = 2; $r <= $row; $r++) {
+                $item = [];
+                for ($c = 1; $c <= $col; $c++) {
+                    $item[] = $sheet->getCellByColumnAndRow($c, $r)->getValue() ?? '';
+                }
+                $data[] = array_combine($fields, $item);
+            }
+            if ($model) {
+                $model->saveAll($data);
+            } else {
+                return $data;
+            }
+        } catch (Throwable $e) {
+            throw new Exception('操作失败 请按照模板中的格式进行导入');
+        }
     }
 
-    public function export()
+
+    public function excel_output($array)
     {
         header('Cache-Control: max-age=0');
         header('Content-Disposition: attachment;filename="excel.xlsx"');
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         include_once getcwd() . '/vendor/PHPExcel/PHPExcel.php';
-        $excel = new \PHPExcel();
-        $excel->getActiveSheet()->fromArray(m('recode')->where(['id' => ['between', implode(',', i(''))]])->select());
-        \PHPExcel_IOFactory::createWriter($excel, 'Excel2007')->save('php://output');
+        $excel = new PHPExcel();
+        $excel->getActiveSheet()->fromArray($array);
+        PHPExcel_IOFactory::createWriter($excel, 'Excel2007')->save('php://output');
+    }
+
+    public function test($p)
+    {
+        $ext = pathinfo($p, PATHINFO_EXTENSION);
+        if (!in_array($ext, ['csv', 'xls', 'xlsx'])) {
+            throw new Exception('未知的数据格式');
+        }
+        if ($ext === 'csv') {
+            $file = fopen($p, 'r');
+            $p    = tempnam(sys_get_temp_dir(), 'import_csv');
+            $fp   = fopen($p, "w");
+            $n    = 0;
+            while ($line = fgets($file)) {
+                $line     = rtrim($line, "\n\r\0");
+                $encoding = mb_detect_encoding($line, ['utf-8', 'gbk', 'latin1', 'big5']);
+                if ($encoding != 'utf-8') {
+                    $line = mb_convert_encoding($line, 'utf-8', $encoding);
+                }
+                if ($n == 0 || preg_match('/^".*"$/', $line)) {
+                    fwrite($fp, $line . "\n");
+                } else {
+                    fwrite($fp, '"' . str_replace(['"', ','], ['""', '","'], $line) . "\"\n");
+                }
+                $n++;
+            }
+            fclose($file) || fclose($fp);
+            $reader = new Csv();
+        } elseif ($ext === 'xls') {
+            $reader = new Xls();
+        } else {
+            $reader = new Xlsx();
+        }
+        return $reader;
     }
 }
