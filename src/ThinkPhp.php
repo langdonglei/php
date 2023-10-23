991f302dfd1e3122c6b84c5ae4866bd305d2e943
langdonglei <langdonglei@icloud.com>
2023-10-23 11:51:43 +0800

    v

--- src/ThinkPhp.php
+++ src/ThinkPhp.php
@@ -2,14 +2,27 @@
 
 namespace langdonglei;
 
+use Exception;
+use think\Validate;
+use think\Config;
+
 class ThinkPhp
 {
     public static function validate($rule, $message = [])
     {
-        $validate = new \think\Validate($rule, $message);
+        $validate = new Validate($rule, $message);
         $validate->check(input());
         if (!$validate) {
-            throw new \Exception($validate->getError());
+            throw new Exception($validate->getError());
         }
+    }
+
+    public static function config($name)
+    {
+        $r = Config($name);
+        if ($r) {
+            return $r;
+        }
+        throw new Exception("config not found $name");
     }
 }
