a16f429748e0c0eb07b54539f85d615947873633
langdonglei <langdonglei@icloud.com>
2023-11-24 15:59:44 +0800

    v

--- src/ThinkPhp.php
+++ src/ThinkPhp.php
@@ -3,6 +3,9 @@
 namespace langdonglei;
 
 use Exception;
+use think\exception\HttpResponseException;
+use think\Request;
+use think\Response;
 use think\Validate;
 use think\Config;
 
@@ -11,10 +14,21 @@ class ThinkPhp
     public static function validate($rule, $message = [])
     {
         $validate = new Validate($rule, $message);
-        $validate->check(input());
-        if (!$validate) {
-            throw new Exception($validate->getError());
+        $validate->check(Request::instance()->param());
+        $error = $validate->getError();
+        if ($error) {
+            throw new HttpResponseException(Response::create([
+                'code' => 0,
+                'msg'  => $error,
+                'time' => Request::instance()->server('REQUEST_TIME'),
+                'data' => null
+            ], 'json'));
         }
+        $r = [];
+        foreach (array_keys($rule) as $key) {
+            $r[$key] = Request::instance()->param($key);
+        }
+        return $r;
     }
 
     public static function config($name, $exception = true)
